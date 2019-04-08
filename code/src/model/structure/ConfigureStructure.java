00e2ed9184be6a940e16664c0186360513772a0a
formation <formation@ubuntu.ubuntu-domain>
2019-04-08 15:55:26 +0200

    Mise à jour du gitignore

--- code/src/model/structure/ConfigureStructure.java
+++ code/src/model/structure/ConfigureStructure.java
@@ -5,17 +5,23 @@ public class ConfigureStructure {
 	public int intersectionNb;
 	public int lineNb;
 	public int columnNb;
+	int laneSize;
+	int sideWalkSize;
 	
 	//Constructeurs
 	public ConfigureStructure() {
 		this.intersectionNb = 1;
 		this.lineNb = 350;
 		this.columnNb = 350;
+		this.laneSize = 5;
+		this.sideWalkSize = 3;
 	}
 	
 	public ConfigureStructure(int size) {
 		this.intersectionNb = 1;
 		this.lineNb = size;
 		this.columnNb = size;
+		this.laneSize = 5;
+		this.sideWalkSize = 3;
 	}
 }
